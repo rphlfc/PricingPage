b0615e7653182a5325d4f9cf235e55e85a28512c
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-17 18:11:28 -0300

    Initial Commit

---
+++ PricingPage/PricingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PricingPageApp.swift
+//  PricingPage
+//
+//  Created by Raphael Cerqueira on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PricingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
