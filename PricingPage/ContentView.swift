c2e071f455952914aa72627bb7a4e5e98c5b8e83
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-17 20:00:55 -0300

    Primeiro commit

--- PricingPage/ContentView.swift
+++ PricingPage/ContentView.swift
@@ -8,10 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    let texts = [
+        "All limited links",
+        "Chat support",
+        "Set your rates"
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 15) {
+            Text("Most popular plan")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .frame(height: 50)
+                .foregroundColor(Color(#colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)))
+                .background(Capsule().fill(Color(#colorLiteral(red: 0.9508243203, green: 0.9648281932, blue: 0.986343205, alpha: 1))))
+            
+            Text("Basic")
+                .font(.title)
+            
+            HStack(spacing: 4) {
+                Text("$19")
+                    .font(.system(size: 29))
+                    .fontWeight(.bold)
+                
+                Text("/month")
+                    .font(.system(size: 23))
+                    .foregroundColor(.gray)
+            }
+            
+            Text("For most business that want to optimize web queries.")
+            
+            Divider()
+                .padding(.top, 20)
+            
+            ForEach(texts, id: \.self) { text in
+                HStack {
+                    Image(systemName: "checkmark")
+                        .foregroundColor(Color.green)
+                        .padding(8)
+                        .background(Color(#colorLiteral(red: 0.853130281, green: 0.9689951539, blue: 0.9230955243, alpha: 1)))
+                        .clipShape(Circle())
+                    
+                    Text(text)
+                }
+            }
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Text("Get started")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 70)
+                    .foregroundColor(.white)
+                    .background(Color(#colorLiteral(red: 0.2415703833, green: 0.8111765981, blue: 0.9954975247, alpha: 1)))
+                    .cornerRadius(8)
+            })
+        }
+        .padding()
     }
+    
+    // DONE
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ README.md
@@ -0,0 +1,24 @@
+# Tela de preço usando SwiftUI 
+Orientação para a criação de uma tela de preços que [encontrei no Instagram](https://www.instagram.com/p/CO-E2a0I_aE/?utm_source=ig_web_copy_link) e decidi implementar usando SwiftUI.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/118566507-667f1700-b74a-11eb-9c50-6ef23c851733.png" width="400">
+</p>
+
+## Vídeos
+- [Vídeo no YouTube](https://youtu.be/ChBItPKyoEY)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
+
